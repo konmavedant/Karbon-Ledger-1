e021f1189dcad46c5352894307cc32756b896a3a
Cryptonean <faizanvit8@gmail.com>
2024-12-30 13:52:22 +0530

    fmt and aiken build

--- client/types/cardano.ts
+++
@@ -1,9 +0,0 @@
-import { WalletApi } from "@lucid-evolution/lucid";
-
-export type Wallet = {
-  name: string;
-  icon: string;
-  apiVersion: string;
-  enable(): Promise<WalletApi>;
-  isEnabled(): Promise<boolean>;
-};
